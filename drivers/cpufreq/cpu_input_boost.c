1e96f3cded09276213ca12da531b8c8d540de766
LibXZR <xzr467706992@163.com>
2020-12-24 22:46:20 +0800

    cpu_input_boost: Add support for max boost freqs lower than min freq
    * Ya it make no sense to boost to a lower freq than min freq
    
    [KyuoFoxHuyu: Rebase for msm-4.9]
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -96,10 +96,10 @@ static unsigned int get_max_boost_freq(struct cpufreq_policy *policy)
 	unsigned int freq;
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
-		freq = max_boost_freq_little;
+		freq = max(max_boost_freq_little, cpu_freq_min_little);
 	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
-		freq = input_boost_freq_big;
-	return min(freq, policy->max);
+		freq = max(max_boost_freq_big, cpu_freq_min_big);
+		return min(freq, policy->max);
 }
 
 static unsigned int get_min_freq(struct cpufreq_policy *policy)
