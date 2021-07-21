0c46b10442d81d474939e20d7db5e8eb361381e2
Emanuel <emanuelghub@gmail.com>
2021-07-21 23:59:12 +0400

    fix

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -878,7 +878,7 @@ static int loop_prepare_queue(struct loop_device *lo)
 {
 	kthread_init_worker(&lo->worker);
 	lo->worker_task = kthread_run_perf_critical(
-			loop_kthread_worker_fn, &lo->worker,
+			kthread_worker_fn, &lo->worker,
 			"loop%d", lo->lo_number);
 	if (IS_ERR(lo->worker_task))
 		return -ENOMEM;
