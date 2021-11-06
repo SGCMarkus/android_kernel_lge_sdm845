ae9053e51eb637fddc03827a1b31b42b8feaac5b
Emanuel <emanuelghub@gmail.com>
2021-11-06 22:09:17 +0400

    Revert "cpuset: Move cpuset override values to Kconfig"
    This reverts commit c84c53e7fdfda4c780ddda59f8cb5ff54845a32f.

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1798,14 +1798,6 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 {
 #ifdef CONFIG_CPUSET_ASSIST
 	static struct cs_target cs_targets[] = {
-		{ "audio-app",		CONFIG_CPUSET_AUDIO_APP },
-		{ "background",		CONFIG_CPUSET_BG },
-		{ "camera-daemon",	CONFIG_CPUSET_CAMERA },
-		{ "display",		CONFIG_CPUSET_DISPLAY },
-		{ "foreground",		CONFIG_CPUSET_FG },
-		{ "restricted",		CONFIG_CPUSET_RESTRICTED },
-		{ "system-background",	CONFIG_CPUSET_SYSTEM_BG },
-		{ "top-app",		CONFIG_CPUSET_TOP_APP },
 	};
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;
