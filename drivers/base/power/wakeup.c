a011f98d38d12ca8dd46a61482a9a0edc201d833
Emanuel <emanuelghub@gmail.com>
2021-11-06 16:23:26 +0400

    wakeup: Use procfs when debugfs is unavailable

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -14,6 +14,7 @@
 #include <linux/suspend.h>
 #include <linux/seq_file.h>
 #include <linux/debugfs.h>
+#include <linux/proc_fs.h>
 #include <linux/pm_wakeirq.h>
 #include <linux/types.h>
 #include <trace/events/power.h>
@@ -1052,7 +1053,7 @@ void pm_wakep_autosleep_enabled(bool set)
 }
 #endif /* CONFIG_PM_AUTOSLEEP */
 
-static struct dentry *wakeup_sources_stats_dentry;
+static struct proc_dir_entry *wakeup_sources_stats_dentry;
 
 /**
  * print_wakeup_source_stats - Print wakeup source statistics information.
@@ -1235,17 +1236,11 @@ static const struct file_operations wakeup_sources_active_stats_fops = {
 };
 #endif
 
-static int __init wakeup_sources_debugfs_init(void)
+static int __init wakeup_sources_proc_init(void)
 {
-	wakeup_sources_stats_dentry = debugfs_create_file("wakeup_sources",
-			S_IRUGO, NULL, NULL, &wakeup_sources_stats_fops);
-
-#ifdef CONFIG_LGE_PM
-	wakeup_sources_stats_dentry = debugfs_create_file(
-		"wakeup_sources_active", S_IRUGO, NULL,
-		NULL, &wakeup_sources_active_stats_fops);
-#endif
+	wakeup_sources_stats_dentry = proc_create("wakelocks", S_IRUGO, NULL,
+			&wakeup_sources_stats_fops);
 	return 0;
 }
 
-postcore_initcall(wakeup_sources_debugfs_init);
+postcore_initcall(wakeup_sources_proc_init);
